a2acf65e0976c64f52567350543a5ca91065b606
ywett02 <ivet.jurcikova@gmail.com>
2025-08-20 17:10:24 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm") version "2.1.21"
+}
+
+group = "com.example.assignment"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package com.example.assignment
+
+fun main() {
+    println("Hello World!")
+}
