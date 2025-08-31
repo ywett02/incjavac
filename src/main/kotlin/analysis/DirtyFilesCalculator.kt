58eeda232226f39cfe66152d871c9e1607de2ef9
ywett02 <ivet.jurcikova@gmail.com>
2025-08-31 14:13:35 +0100

    Implement DirtyFilesCalculator

---
+++ src/main/kotlin/analysis/DirtyFilesCalculator.kt
@@ -0,0 +1,34 @@
+package com.example.assignment.analysis
+
+import com.example.assignment.entity.FileChanges
+import com.example.assignment.entity.FqName
+import com.example.assignment.storage.DependencyMapInMemoryStorage
+import com.example.assignment.storage.FileToFqnMapInMemoryStorage
+import com.example.assignment.util.inverted
+import java.io.File
+
+class DirtyFilesCalculator(
+    private val fileToFqnMapInMemoryStorage: FileToFqnMapInMemoryStorage,
+    private val dependencyMapInMemoryStorage: DependencyMapInMemoryStorage
+) {
+    fun calculateDirtyFiles(changes: FileChanges): Set<File> {
+        val fileToFqnMap: Map<File, Set<FqName>> = fileToFqnMapInMemoryStorage.get()
+        val invertedDependencyMap: Map<FqName, Set<FqName>> = dependencyMapInMemoryStorage.get().inverted()
+        val fqnToFileMap: Map<FqName, Set<File>> = fileToFqnMap.inverted()
+
+        val sourceFiles = changes.addedAndModifiedFiles + changes.removedFiles
+        return sourceFiles
+            .asSequence()
+            .flatMap { sourceFile: File ->
+                fileToFqnMap.getOrDefault(sourceFile.absolutePath, emptySet())
+            }
+            .flatMap { classFqnName ->
+                invertedDependencyMap.getOrDefault(classFqnName, emptySet())
+            }
+            .flatMap { dependencyFqnName ->
+                fqnToFileMap.getOrDefault(dependencyFqnName, emptySet())
+            }
+            .plus(changes.addedAndModifiedFiles)
+            .toSet()
+    }
+}
