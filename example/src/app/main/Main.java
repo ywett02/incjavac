da4aa203a418d248ab226e3e137a08caad2c20a3
ywett02 <ivet.jurcikova@gmail.com>
2025-09-01 15:29:30 +0100

    Improve provided example

---
+++ example/src/app/main/Main.java
@@ -0,0 +1,10 @@
+package com.example.app.main;
+
+import com.example.app.Greeter;
+
+public class Main {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        greeter.greet("Ivet");
+    }
+}
