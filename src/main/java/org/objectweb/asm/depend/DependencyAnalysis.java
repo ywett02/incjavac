c6f883fbedcda18510e385e064ade9b093562940
ywett02 <ivet.jurcikova@gmail.com>
2025-09-22 09:19:57 +0100

    Implement transitive supertypes analysis

---
+++ src/main/java/org/objectweb/asm/depend/DependencyAnalysis.java
@@ -0,0 +1,25 @@
+package org.objectweb.asm.depend;
+
+import com.example.assignment.entity.FqName;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class DependencyAnalysis {
+
+    protected Set<FqName> types;
+    protected Set<FqName> superTypes;
+
+    public DependencyAnalysis() {
+        types = new HashSet<>();
+        superTypes = new HashSet<>();
+    }
+
+    public Set<FqName> getTypes() {
+        return Set.copyOf(types);
+    }
+
+    public Set<FqName> getSuperTypes() {
+        return Set.copyOf(superTypes);
+    }
+}
